b1b1a2d5a3d760c88a378b5947d7e050f54e1c21
R-bit548 <82327506+R-bit548@users.noreply.github.com>
2024-12-19 15:34:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -162,8 +162,8 @@ app.use((err, req, res, next) => {
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Serving on port ${port}`)
-})
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on port 3000');
+});
 
 
